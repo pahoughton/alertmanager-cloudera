456c2027c38cf8c2803fa0f0cddcbcfe3067bc18
Paul Houghton <paul4hough@gmail.com>
2019-02-08 05:18:41 -0700

    add send to alertmanager

--- alertmanager/alertmanager.go
+++ alertmanager/alertmanager.go
@@ -4,10 +4,17 @@
 package alertmanager
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"net/http"
 
 	"github.com/pahoughton/cloudera-amgr-alert/config"
+)
+
+const (
+	amgrEndpoint	= "/api/v1/alerts"
+	contTypeJSON	= "application/json"
 )
 
 type AmgrAlert struct {
@@ -19,11 +26,44 @@ type AmgrAlert struct {
 
 func SendAlerts(amaList []AmgrAlert, cfg *config.Config,debug bool) error {
 
-	amjson, err := json.MarshalIndent(amaList,"","  ")
+	if debug {
+		djson, err := json.MarshalIndent(amaList,"","  ")
+		if err != nil {
+			return err
+		}
+		fmt.Println(string(djson))
+	}
+	amjson, err := json.Marshal(amaList)
 	if err != nil {
 		return err
 	}
-	fmt.Printf("%s\n",string(amjson))
 
-	return fmt.Errorf("alertmanager.sendAlert error")
+	for _, amgr := range cfg.Amgrs {
+		for _, targ := range amgr.SConfigs.Targets {
+			url := fmt.Sprintf("%s://%s%s",amgr.Scheme, targ, amgrEndpoint)
+
+			if debug {
+				fmt.Println("DEBUG: amgr url - ", url)
+			} else {
+				resp, err := http.Post(
+					url,
+					contTypeJSON,
+					bytes.NewBuffer(amjson))
+				if err != nil {
+					return err
+				}
+				defer resp.Body.Close()
+				if resp.StatusCode != 200 {
+					return fmt.Errorf("alertmanager status %s\n%s",
+						resp.Status,
+						string(amjson))
+				} else {
+					fmt.Printf("DEBUG: sent %s\n",string(amjson))
+				}
+			}
+
+		}
+	}
+
+	return nil
 }
